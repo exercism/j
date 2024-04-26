c2db86a1bb271f78cc6f13658b77d9264f4724b7
Erik Schierboom <erik_schierboom@hotmail.com>
2024-04-26 10:55:03 +0200

    Don't hardcode the jconsole path

--- bin/verify-exercises.sh
+++ bin/verify-exercises.sh
@@ -32,7 +32,7 @@ exit_code=0
 for practice_exercise_dir in ./exercises/practice/*/; do
     if [ -d $practice_exercise_dir ]; then
         echo "Checking $(basename "${practice_exercise_dir}") exercise..."
-        res=$(/opt/j9.5/bin/jconsole bin/test_exercise.ijs "$practice_exercise_dir/")
+        res=$(jconsole bin/test_exercise.ijs "$practice_exercise_dir/")
         if [ $? -ne 0 ]; then 
             echo "Error in ${practice_exercise_dir}"
             ((exit_code++))
